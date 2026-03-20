a99721f13d1bb016df19ae7965682b0cf8e126ae
cristell calderon <calderoncristell948@gmail.com>
2026-03-20 12:26:21 -0500

    supabse

---
+++ middleware.ts
@@ -0,0 +1,19 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+
+const isPublicRoute = createRouteMatcher([
+  '/sign-in(.*)',
+  '/sign-up(.*)',
+])
+
+export default clerkMiddleware(async (auth, request) => {
+  if (!isPublicRoute(request)) {
+    await auth.protect()
+  }
+})
+
+export const config = {
+  matcher: [
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jls|map|ts|tsx|ico|png|jpg|jpeg|gif|svg|woff2?|ttf|eot)).*)',
+    '/(api|trpc)(.*)',
+  ],
+}
